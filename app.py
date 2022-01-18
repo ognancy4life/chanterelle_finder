9045b04cb2199e4f113b95ad932017508346e2d2
Nancy Williams <59902324+ognancy4life@users.noreply.github.com>
2022-01-17 20:36:32 -0800

    Add title

--- app.py
+++ app.py
@@ -2,6 +2,13 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+st.set_page_config(page_icon="mushroom", layout="wide")
+st.title("Chanterelle Finder")
+st.subheader("by Nancy Williams")
+st.write('This app ... using Streamlit on a Heroku app.')
+#st.caption("*Note that ...")
+
+
 df = pd.DataFrame(
      np.random.randn(1000, 2) / [50, 50] + [37.76, -122.4],
      columns=['lat', 'lon'])
